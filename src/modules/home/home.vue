edb4d3e811c8733c1a011968ef9485e761d36537
zhen.liu <zhen.liu@chukong-inc.com>
2019-01-04 00:34:20 +0800

    time

--- src/modules/home/home.vue
+++ src/modules/home/home.vue
@@ -316,7 +316,7 @@ export default {
         if (that.trade_right) {
           that.queryTransList();
         }
-      }, 2000);
+      }, 4000);
     },
     queryBlockList() {
       const that = this;
