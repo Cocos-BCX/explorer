68a98b7ce9e72816d7865fc0bd3f92c7d1f82580
justoxh <41144545+justoxh@users.noreply.github.com>
2019-07-24 15:44:46 +0800

    update main.js
    author message

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 // import Vue from 'vue'
+// author cocos-bcx team 
 import App from './App'
 import router from './router'
 import VueI18n from 'vue-i18n'
