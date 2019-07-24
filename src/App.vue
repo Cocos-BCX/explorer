85b89b673576f6805ac94babf06732643bc6b031
sdliuzhen <13276381265@163.com>
2019-07-24 10:46:41 +0800

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,6 @@
 <template>
   <div id="app">
     <router-view/>
-    <!-- <el-dialog :title="$t('home.prompt')" :visible.sync="dialogVisible" width="40%">
-      <span>{{$t('home.content')}}</span>
-      <span slot="footer" class="dialog-footer">
-        <el-button @click="dialogVisible = false">{{$t('home.button.cancel')}}</el-button>
-        <el-button type="primary" @click="dialogVisible = false">{{$t('home.button.confirm')}}</el-button>
-      </span>
-    </el-dialog>-->
   </div>
 </template>
 
@@ -23,12 +16,6 @@ export default {
 </script>
 <style>
 #app {
-  /* font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px; */
   @import "./style/common.css";
 }
 </style>
